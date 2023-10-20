6b0818e6ede349f0d257ca15562a9d5ce0b26bac
O-suke12 <souke0504@gmail.com>
2023-10-21 08:12:30 +0900

    Almost done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RL_scratch
 
+## Reference
+
+PPO: https://medium.com/analytics-vidhya/coding-ppo-from-scratch-with-pytorch-part-1-4-613dfc1b14c8
+
+Vectorized-PPo: https://www.youtube.com/watch?v=MEt6rrxH8W4&list=PLD80i8An1OEHhcxclwq8jOMam0m0M9dQ_&index=1
+
+REINFORCE: https://huggingface.co/learn/deep-rl-course/unit4/introduction 
--- ppo.py
+++ ppo.py
@@ -40,7 +40,8 @@ def parse_args():
         help="the entity (team) of wandb's project")
     parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="weather to capture videos of the agent performances (check out `videos` folder)")
-    
+    parser.add_argument("--num-episodes", type=int, default=10,
+        help="the number of episodes to run")
     parser.add_argument("--num-envs", type=int, default=4,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
@@ -71,6 +72,10 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
+    parser.add_argument("--num_updates", type=int, default=5,
+        help="the number of update in each episode")
+    parser.add_argument("--clip", type=float, default=0.2,
+        help="the value of clip")
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
@@ -84,49 +89,116 @@ def layer_init(layer, std=np.sqrt(2), bias=0.0):
         return layer
 
 class Agent(nn.Module):
-    def __init__(self, envs):
+    def __init__(self, env):
         super().__init__()
         self.critic = nn.Sequential(
-            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            layer_init(nn.Linear(np.array(env.observation_space.shape).prod(), 64)),
             nn.Tanh(),
             layer_init(nn.Linear(64,64)),
             nn.Tanh(),
             layer_init(nn.Linear(64,1), std=1.),
         )
         self.actor = nn.Sequential(
-            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            layer_init(nn.Linear(np.array(env.observation_space.shape).prod(), 64)),
             nn.Tanh(),
             layer_init(nn.Linear(64,64)),
             nn.Tanh(),
-            layer_init(nn.Linear(64,envs.single_action_space.n), std=0.01),
+            layer_init(nn.Linear(64,env.action_space.n), std=0.01),
         )
 
     def get_value(self, x):
-        return self.critic(x)
+        x = x.clone().detach()
+        return self.critic(x).squeeze()
     
-    def get_action_and_value(self, x, action=None):
-        logits = self.actor(x)
+    def get_action_and_prob(self, x, action=None):
+        logits = self.actor(torch.from_numpy(x.astype(np.float32)))
         probs = Categorical(logits=logits)
         if action is None:
             action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy, self.critic(x)
+        return action.numpy(), probs.log_prob(action)
     
+    def evaluate(self, obs, acts):
+        logits = self.actor(obs)
+        probs = Categorical(logits=logits)
+        return self.critic(obs).squeeze(), probs.log_prob(acts)
+
+class PPO():
+    def __init__(self, env):
+        super().__init__()
+        self.env = env
+        self.actor_optim = torch.optim.Adam(agent.actor.parameters(), lr=args.learning_rate)
+        self.critic_optim = torch.optim.Adam(agent.critic.parameters(), lr=args.learning_rate)
     
+    def roll_out(self):
+        obs = []
+        rews = []
+        acts = []
+        log_probs = []
+        lens = []
+        rtgs = []
+
+        observation = self.env.reset()[0]
+        done = False 
+        ep_rews = []
+        for _ in range(args.num_steps):
+            obs.append(observation)
+            action, log_prob = agent.get_action_and_prob(observation)
+            observation, reward, done, truncated, info = env.step(action)
+            acts.append(action) #self.acts = np.append(self.acts, action)# 
+            log_probs.append(log_prob)
+            ep_rews.append(reward)
+            if done:
+                break
+        obs = np.array(obs).reshape(-1, 4)
+        log_probs = torch.FloatTensor(log_probs)
+        obs = torch.FloatTensor(obs)
+        acts = torch.FloatTensor(np.array(acts))
+        lens.append(info["episode"]["l"])
+        rews.append(ep_rews)
+        rtgs = torch.FloatTensor(self.to_go(rews))  # rtgs: nd_array, log_probs: torch_tensor
+        return obs, acts, rtgs, log_probs
+
+    def to_go(self, rewards):
+        rtgs = []
+        disc = 0
+        for ep_rew in reversed(rewards):
+            disc = 0
+            for rew in reversed(ep_rew):
+                disc = rew + args.gamma * disc
+                rtgs.insert(0, disc)
+        return np.array(rtgs)
+    
+    def advantage_estimate(self, obs, rtgs):
+        values = []
+        values = agent.get_value(obs)
+        advantages = rtgs - values
+        advantage = (advantages - advantages.mean())/(advantages.std() - 1e-10)
+        return advantages
+    
+    def train(self):
+        for _ in range(args.num_episodes):
+            obs, acts, rtgs, log_probs = self.roll_out()
+            advantages = self.advantage_estimate(obs, rtgs)
+            for _ in range(args.num_updates):
+                V, curr_log_probs = agent.evaluate(obs, acts)
+                pi_ratios = torch.exp(curr_log_probs - log_probs)
+                surrogate1 = pi_ratios*advantages
+                surrogate2 = torch.clamp(pi_ratios, 1-args.clip, 1+args.clip)*advantages
+                actor_loss = (-torch.min(surrogate1, surrogate2)).mean()
+                self.actor_optim.zero_grad()
+                actor_loss.backward()
+                self.actor_optim.step()
+                
+                loss = nn.MSELoss()
+                critic_loss = loss(V, rtgs)
+                self.critic_optim.zero_grad()
+                critic_loss.backward()
+                self.critic_optim.step()
+
+
 if __name__ == "__main__":
     args = parse_args()
     run_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.track:
-        import wandb
-
-        wandb.init(
-            project=args.wandb_project_name,
-            entity=args.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(args),
-            name=run_name,
-            monitor_gym=True,
-            save_code=True,
-        )
     writer = SummaryWriter(f"runs/{run_name}")
     writer.add_text(
         "hyperparameters",
@@ -139,14 +211,10 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-
     env = gym.make(args.gym_id)
     env = RecordEpisodeStatistics(env)
-    observation = env.reset()
-    for _ in range(200):
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            observation = env.reset()
+    agent = Agent(env)
+    ppo = PPO(env)
+    ppo.train()
             
 
