c33cc299b979884475c95a56865cf573433571b7
O-suke12 <souke0504@gmail.com>
2023-10-23 20:19:42 +0900

    add learning rate annealing and mini-batch update

--- ppo.py
+++ ppo.py
@@ -2,9 +2,10 @@ import argparse
 import datetime
 import os
 import random
+import time
 from distutils.util import strtobool
 
-import gym
+import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
@@ -23,15 +24,6 @@ def parse_args():
     )
     parser.add_argument("--gym-id", type=str, default="CartPole-v1", help="the id of the gym environment")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4, help="the learning rate of the optimizer")
-    parser.add_argument("--seed", type=int, default=1, help="seed of the experiment")
-    parser.add_argument(
-        "--torch-deterministic",
-        type=lambda x: bool(strtobool(x)),
-        default=True,
-        nargs="?",
-        const=True,
-        help="if toggled, `torch.backends.cudnn.deterministic=False`",
-    )
     parser.add_argument(
         "--cuda",
         type=lambda x: bool(strtobool(x)),
@@ -40,53 +32,18 @@ def parse_args():
         const=True,
         help="if toggled, cuda will be enabled by default",
     )
-    parser.add_argument("--num-episodes", type=int, default=100, help="the number of episodes to run")
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--num-episodes", type=int, default=4000, help="the number of episodes to run")
     parser.add_argument(
         "--num-steps", type=int, default=128, help="the number of steps to run in each environment per policy rollout"
     )
-    parser.add_argument(
-        "--anneal-lr",
-        type=lambda x: bool(strtobool(x)),
-        default=True,
-        nargs="?",
-        const=True,
-        help="Toggle learning rate annealing for policy and value networks",
-    )
-    parser.add_argument(
-        "--gae",
-        type=lambda x: bool(strtobool(x)),
-        default=True,
-        nargs="?",
-        const=True,
-        help="Use GAE for advantage computation",
-    )
     parser.add_argument("--gamma", type=float, default=0.99, help="the discount factor gamma")
-    parser.add_argument(
-        "--gae-lambda", type=float, default=0.95, help="the lambda for the general advantage estimation"
-    )
-    parser.add_argument("--num-minibatches", type=int, default=4, help="the number of mini-batches")
-    parser.add_argument(
-        "--norm-adv",
-        type=lambda x: bool(strtobool(x)),
-        default=True,
-        nargs="?",
-        const=True,
-        help="Toggles advantages normalization",
-    )
-    parser.add_argument("--clip-coef", type=float, default=0.2, help="the surrogate clipping coefficient")
-    parser.add_argument(
-        "--clip-vloss",
-        type=lambda x: bool(strtobool(x)),
-        default=True,
-        nargs="?",
-        const=True,
-        help="Toggles whether or not to use a clipped loss for the value function, as per the paper.",
-    )
     parser.add_argument("--num-updates", type=int, default=4, help="the number of update in each episode")
     parser.add_argument("--clip", type=float, default=0.2, help="the surrogate clipping coefficient")
     parser.add_argument(
         "--render", type=lambda x: bool(strtobool(x)), default=False, help="if toggled, render the environment"
     )
+    parser.add_argument("--num-minibatches", type=int, default=6)
     args = parser.parse_args()
     return args
 
@@ -129,7 +86,7 @@ class Agent(nn.Module):
     def evaluate(self, obs, acts):
         logits = self.actor(obs)
         probs = Categorical(logits=logits)
-        return self.critic(obs).squeeze(), probs.log_prob(acts)
+        return self.critic(obs).squeeze(), probs.log_prob(acts),  probs.entropy()
 
 
 class PPO:
@@ -153,7 +110,7 @@ class PPO:
             obs.append(observation)
             action, log_prob = agent.get_action_and_prob(observation)
             observation, reward, done, truncated, info = env.step(action)
-            acts.append(action)  # self.acts = np.append(self.acts, action)#
+            acts.append(action) 
             log_probs.append(log_prob)
             ep_rews.append(reward)
             if done:
@@ -162,8 +119,7 @@ class PPO:
         log_probs = torch.FloatTensor(log_probs)
         obs = torch.FloatTensor(obs)
         acts = torch.FloatTensor(np.array(acts))
-        # lens.append(info["episode"]["l"])
-        rtgs = torch.FloatTensor(self.to_go(ep_rews))  # rtgs: nd_array, log_probs: torch_tensor
+        rtgs = torch.FloatTensor(self.to_go(ep_rews)) 
         return (
             obs,
             acts,
@@ -174,8 +130,6 @@ class PPO:
     def to_go(self, rewards):
         rtgs = []
         disc = 0
-        # for ep_rew in reversed(rewards):
-        #     disc = 0
         for rew in reversed(rewards):
             disc = rew + args.gamma * disc
             rtgs.insert(0, disc)
@@ -189,29 +143,54 @@ class PPO:
         return advantages
 
     def train(self):
+        total_upd = args.num_episodes*args.num_updates
         for epi in range(args.num_episodes):
+            if epi%100==0:
+                print(f"{epi}\n")
             obs, acts, rtgs, log_probs = self.roll_out()
             advantages = self.advantage_estimate(obs, rtgs)
             advantages = advantages.detach()
-            for upd in range(args.num_updates):
-                total_upd = epi * args.num_updates + upd
-                V, curr_log_probs = agent.evaluate(obs, acts)
-                pi_ratios = torch.exp(curr_log_probs - log_probs)
-                surrogate1 = pi_ratios * advantages
-                surrogate2 = torch.clamp(pi_ratios, 1 - args.clip, 1 + args.clip) * advantages
-                actor_loss = (-torch.min(surrogate1, surrogate2)).mean()
-                self.actor_optim.zero_grad()
-                actor_loss.backward()
-                self.actor_optim.step()
 
-                loss = nn.MSELoss()
-                critic_loss = loss(V, rtgs)
-                self.critic_optim.zero_grad()
-                critic_loss.backward()
-                self.critic_optim.step()
-                writer.add_scalar("loss-actor", actor_loss, total_upd)
-                writer.add_scalar("loss-critic", critic_loss, total_upd)
-            writer.add_scalar("epi-length", len(rtgs), total_upd)
+            step = obs.size(0)
+            inds = np.arange(step)
+            minibatch_size = step//args.num_minibatches
+            for upd in range(args.num_updates):
+                sofar_upd = epi * args.num_updates + upd
+
+                #learning rate annealing which doesn't work well but it is good with mini-batch
+                frac = (sofar_upd - 1.0) / total_upd
+                new_lr = args.learning_rate*(1.0-frac)
+                self.actor_optim.param_groups[0]["lr"] = new_lr
+                self.critic_optim.param_groups[0]["lr"] = new_lr
+
+                #mini-batch works significantly well
+                np.random.shuffle(inds)
+                for start in range(0, step, minibatch_size):
+                    end = start + minibatch_size
+                    idx = inds[start:end]
+                    mini_obs = obs[idx]
+                    mini_acts = acts[idx]
+                    mini_log_probs = log_probs[idx]
+                    mini_advantages = advantages[idx]
+                    mini_rtgs = rtgs[idx]
+                    V, curr_log_probs, entropy = agent.evaluate(mini_obs, mini_acts)
+                    pi_ratios = torch.exp(curr_log_probs - mini_log_probs)
+                    surrogate1 = pi_ratios * mini_advantages
+                    surrogate2 = torch.clamp(pi_ratios, 1 - args.clip, 1 + args.clip) * mini_advantages
+                    actor_loss = (-torch.min(surrogate1, surrogate2)).mean()
+                    self.actor_optim.zero_grad()
+                    actor_loss.backward()
+                    self.actor_optim.step()
+
+                    loss = nn.MSELoss()
+                    critic_loss = loss(V, mini_rtgs)
+                    self.critic_optim.zero_grad()
+                    critic_loss.backward()
+                    self.critic_optim.step()
+
+                writer.add_scalar("loss-actor", actor_loss, sofar_upd)
+                writer.add_scalar("loss-critic", critic_loss, sofar_upd)
+            writer.add_scalar("epi-length", len(rtgs), sofar_upd)
 
 
 if __name__ == "__main__":
@@ -224,24 +203,14 @@ if __name__ == "__main__":
         "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
     )
 
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-    env = gym.make(args.gym_id)
-    # env = gym.wrappers.RecordVideo(env, "video", episode_trigger=lambda x: x % 20 == 0)
+    env = gym.make(args.gym_id, render_mode="rgb_array")
     env = RecordEpisodeStatistics(env)
+    if args.render:
+        env = gym.wrappers.RecordVideo(env, "./video", episode_trigger = lambda x: x % 100 == 0)
     agent = Agent(env)
     ppo = PPO(env)
     ppo.train()
-    if args.render:
-        obs = env.reset()[0]
-        action = agent.get_action_and_prob(obs)[0]
-        while True:
-            obs, reward, done, truncated, info = env.step(action)
-            action = agent.get_action_and_prob(obs)[0]
-            if done:
-                break
-            env.render()
+    env.close()
+
+    
